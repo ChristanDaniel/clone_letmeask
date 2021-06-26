8b751b763935b69e1b2de2db51351965b02c03cc
ChristianDaniel <christian_daniel04@hotmail.com>
2021-06-26 11:22:59 -0300

    error: autenticação com google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,14 +6,23 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+import { auth, firebase } from '../services/firebase'
+
 import '../styles/auth.scss'
 
 export function Home() {
     const history = useHistory();
 
-    function navigateToNewRoom() {
-        history.push('/rooms/new')
-    }
+    function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+        })
+
+
+//        history.push('/rooms/new')
+     }
 
 
     return (
@@ -26,7 +35,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         crie sua sala com o Google
                     </button>
